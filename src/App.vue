b377ea6b45023eee8f62d00976ded7576d0763b0
Lqw981210 <2319944708@qq.com>
2024-10-31 17:16:41 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-header>AIGC</el-header>
+            <el-main>
+                <div class="topArea"></div>
+                <div class="sendArea"></div>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.common-layout {
+    width: 100%;
+    height: 100%;
+
+    .el-container {
+        width: 100%;
+        height: 100%;
+
+        .el-header {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 10%;
+            width: 100%;
+            text-align: center;
+            background-color: rgb(148, 105, 105);
+            color: #FFF;
+            font-size: calc(2vw);
+        }
+
+        .el-main {
+            height: 90%;
+            .topArea {
+                height: auto;
+            }
+            .sendArea{
+            height: 10%;
+            background-color: red;
+            }
+        }
+    }
+}
+</style>
